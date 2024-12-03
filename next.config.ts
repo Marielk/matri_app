089cdf093cff096785861eee3d40bd7e02902be5
Marielk <correo@especifico.com>
2024-12-03 11:34:44 -0300

    remove default page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/nombre-del-repositorio", // Reemplaza con el nombre de tu repositorio
+  trailingSlash: true,
 };
 
 export default nextConfig;
